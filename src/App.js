273108e47da48beeff1a7eafbb90319d95799c6e
Anthony Djoewanda <16738612+tonychuaco@users.noreply.github.com>
2020-03-31 17:00:28 -0400

    prettier

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class LifeCycle extends Component {
-
   constructor() {
     super();
     this.state = {
@@ -9,37 +8,37 @@ class LifeCycle extends Component {
       isLoaded: false,
     };
     // console.log('constructor');
-  };
+  }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(res => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
         return res.json();
       })
-      .then(data => {
+      .then((data) => {
         // console.log(data);
         this.setState({
           items: data,
           isLoaded: true,
-        })
+        });
       });
     // console.log('componentDidMount()');
   }
 
   render() {
-    var {isLoaded, items} = this.state;
+    var { isLoaded, items } = this.state;
     if (!isLoaded) {
       return <div>Loading....</div>;
     } else {
       // console.log('render()');
-      return(
+      return (
         <div className="App">
           <div className="Names">
             <ul>
-              {items.map(el => {
-                return(
+              {items.map((el) => {
+                return (
                   <li key={el.id}>
-                    Name: {el.name} | UserName: {el.username} | {' '}
+                    Name: {el.name} | UserName: {el.username} |{" "}
                     <a href={`https://${el.website}`}>Website</a>
                   </li>
                 );
@@ -49,7 +48,7 @@ class LifeCycle extends Component {
         </div>
       );
     }
-  };
-};
+  }
+}
 
 export default LifeCycle;
